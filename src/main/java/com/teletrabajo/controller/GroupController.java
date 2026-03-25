a76805ca22dd686ae77df99357468439a477b854
ricardotoledob <ricardo.toledob@gmail.com>
2026-03-25 14:48:18 -0300

    jhajshdjhasd

--- src/main/java/com/teletrabajo/controller/GroupController.java
+++ src/main/java/com/teletrabajo/controller/GroupController.java
@@ -1,4 +1,81 @@
 package com.teletrabajo.controller;
 
+import com.teletrabajo.dto.GroupDTO;
+import com.teletrabajo.service.impl.GroupServiceImpl;
+import com.teletrabajo.service.impl.RoleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/groups")
+@PreAuthorize("hasAnyRole('ADMIN','ADMINISTRATIVO','SUPERVISOR','JEFATURA')")
 public class GroupController {
+
+    @Autowired
+    private GroupServiceImpl service;
+
+    @PostMapping
+    public ResponseEntity<GroupDTO> create(@RequestBody GroupDTO dto) {
+        return ResponseEntity.ok(service.create(dto));
+    }
+
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GroupDTO> getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GroupDTO> update(@PathVariable Integer id, @RequestBody GroupDTO dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<GroupDTO>> getAll() {
+        return ResponseEntity.ok(service.listAll());
+    }
+
+    @GetMapping("/getAllPaginated")
+    public ResponseEntity<Page<GroupDTO>> getAllPaginated(
+            @RequestParam(required = false) String name,
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok(service.getAllPaginated(name, pageable));
+    }
+
+
+    /* SOFT DELETE */
+    @GetMapping
+    public ResponseEntity<List<GroupDTO>> listActive() {
+        return ResponseEntity.ok(service.listActive());
+    }
+
+    @GetMapping("/deleted")
+    public ResponseEntity<List<GroupDTO>> listDeleted() {
+        return ResponseEntity.ok(service.listDeleted());
+    }
+
+
+
+    @PostMapping("/{id}/restore")
+    public ResponseEntity<Void> restore(@PathVariable Integer id) {
+        service.restore(id);
+        return ResponseEntity.noContent().build();
+    }
 }
