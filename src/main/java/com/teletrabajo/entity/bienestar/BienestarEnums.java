4b2801398be808de7258e599492f9d7b42a2e0fe
ricardotoledob <ricardo.toledob@gmail.com>
2026-05-18 05:40:48 -0300

    kjasdjasjkd

--- src/main/java/com/teletrabajo/entity/bienestar/BienestarEnums.java
+++
@@ -1,8 +0,0 @@
-package com.teletrabajo.entity.bienestar;
-
-public class BienestarEnums {
-    public enum PostulationStatus { DRAFT, SUBMITTED, UNDER_REVIEW, OBSERVED, APPROVED, REJECTED, CANCELLED }
-    public enum BeneficiaryType { AFFILIATE, FAMILY_MEMBER }
-    public enum ExpenseCategory { BASIC, EDUCATION, OTHER }
-    public enum DocumentGroup { REQUIRED, ADDITIONAL }
-}
